ae120f36d6420b39047fbf5c8d7d5632f79e20ed
raifakabir <raifakabirdt@gmail.com>
2024-09-12 06:47:36 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(function(){
+    $("#hide").click(function(){
+        $(".box").hide(1000)
+    })
+    $("#show").click(function(){
+        $(".box").show(1000)
+    })
+    $("#hideshow").click(function(){
+        $(".box").toggle(1000)
+    })
+    $("#fadeIn").click(function(){
+        $(".box").fadeIn(1000)
+    })
+
+    $("#fadeOut").click(function(){
+        $(".box").fadeOut(1000)
+    })
+      $("#fadeOutIn").click(function(){
+        $(".box").fadeToggle(1000)
+    })
+    $("#slideUp").click(function(){
+        $(".box").slideUp(1000)
+    })
+    $("#slideDown").click(function(){
+        $(".box").slideDown(1000)
+    })
+    $("#slideUpDown").click(function(){
+        $(".box").slideToggle(1000)
+    })
+    $("#switchColor").click(function(){
+        $(".box").addClass("secondbox")
+
+    })
+    $("#removeColor").click(function(){
+        $(".box").removeClass("secondbox")
+    })
+})
